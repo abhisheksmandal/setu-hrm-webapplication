00c1cc80f820a5d5b2540a5fc0c07069312a51a9
abhisheksmandal <abhishek.s.mandal@gmail.com>
2022-12-14 13:34:39 +0530

    Dashboard layout has been added

--- src/views/Starter.js
+++ src/views/Starter.js
@@ -1,4 +1,19 @@
-import { Col, Row } from "reactstrap";
+import {
+  Col,
+  Row,
+  Container,
+  Card,
+  CardBody,
+  CardTitle,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Table,
+  Button,
+  ButtonGroup,
+} from "reactstrap";
 import SalesChart from "../components/dashboard/SalesChart";
 import Feeds from "../components/dashboard/Feeds";
 import ProjectTables from "../components/dashboard/ProjectTable";
@@ -47,7 +62,7 @@ const BlogData = [
 const Starter = () => {
   return (
     <div>
-      **Top Cards**
+      {/* **Top Cards**
       <Row>
         <Col sm="6" lg="3">
           <TopCards
@@ -114,15 +129,129 @@ const Starter = () => {
             />
           </Col>
         ))}
-      </Row>
-
-
+      </Row> */}
       {/* Dashboard Code Starts Here */}
-      <Row>
-        <Col>
-        
-        </Col>
-      </Row>
+      <Card>
+        <CardBody className="">
+          <Container>
+            <Row>
+              <h1 className="text-center p-4">Welcome, User</h1>
+            </Row>
+            {/* <Row>
+              <Col sm="6" lg="6">
+                <TopCards
+                  bg="bg-light-success text-success"
+                  // title="Profit"
+                  subtitle="Worked in a Month"
+                  earning="_ _ : Hours"
+                  // icon="bi bi-wallet"
+                />
+              </Col>
+              <Col sm="6" lg="6">
+                <TopCards
+                  // bg="bg-light-danger text-danger"
+                  // title="Refunds"
+                  subtitle="Extra in a Month"
+                  earning="_ _ : Hours"
+                  // icon="bi bi-coin"
+                />
+              </Col>
+            </Row> */}
+            <Row className="mt-3 mb-4">
+              <Col sm="6" lg="6">
+                <div className="bg-success p-4 border text-white taskDisplayer">
+                  <h4 className="text-center">_ _ : Hours</h4>
+                  <p className="text-center mb-0">Worked in a Month</p>
+                </div>
+              </Col>
+              <Col sm="6" lg="6">
+                <div className="bg-success p-4 border text-white taskDisplayer">
+                  <h4 className="text-center">_ _ : Hours</h4>
+                  <p className="text-center mb-0">Extra in a Month</p>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm="6" lg="6">
+                <FormGroup>
+                  <Label for="exampleSelect">Projects</Label>
+                  <Input id="exampleSelect" name="select" type="select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Input>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row className="mt-3">
+              <Col sm="6" lg="6">
+                <div className="bg-danger p-4 border text-white taskDisplayer">
+                  Pending
+                </div>
+              </Col>
+              <Col sm="6" lg="6">
+                <div className="bg-warning p-4 border text-white taskDisplayer">
+                  In Progress
+                </div>
+              </Col>
+            </Row>
+            <Row className="mt-3">
+              <Col sm="6" lg="6">
+                <div className="bg-success p-4 border text-white taskDisplayer">
+                  Completed
+                </div>
+              </Col>
+              <Col sm="6" lg="6">
+                <div className="bg-info p-4 border text-white taskDisplayer">
+                  Unallocated
+                </div>
+              </Col>
+            </Row>
+
+            <Row className="mt-4">
+              <Col lg="12">
+                <Table bordered hover>
+                  <tbody>
+                    <tr className="notificationRow">
+                      <th scope="row">1</th>
+                      <td>
+                        <h6>Hemant has applied for a sick leave</h6>
+                      </td>
+                      <td>Half Day Leave</td>
+                      <td>
+                        <span>
+                          <Button
+                            className="btn"
+                            color="buttonColor"
+                            size="sm"
+                            block
+                          >
+                            Review
+                          </Button>
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td>Larry</td>
+                      <td>the Bird</td>
+                      <td>@twitter</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </Container>
+        </CardBody>
+      </Card>
     </div>
   );
 };
