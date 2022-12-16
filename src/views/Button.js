dc69a08a7126d4a6c49488b72ea19a31fe16b66d
abhisheksmandal <abhishek.s.mandal@gmail.com>
2022-12-16 13:58:50 +0530

    Added button structure

---
+++ src/views/Button.js
@@ -0,0 +1,34 @@
+import { Link, useLocation } from "react-router-dom";
+
+const navigation = [
+  {
+    title: "Click Me",
+    href: "/project",
+  },
+];
+
+// const showMobilemenu = () => {
+//   document.getElementById("sidebarArea").classList.toggle("showSidebar");
+// };
+let location = useLocation();
+
+<Nav vertical className="sidebarNav">
+  {navigation.map((navi, index) => (
+    <NavItem key={index} className="sidenav-bg">
+      <Link
+        to={navi.href}
+        className={
+          location.pathname === navi.href
+            ? "text-primary nav-link py-3"
+            : "nav-link text-secondary py-3 primary"
+        }
+      >
+        <div className="button-group">
+          <Button className="btn" color="primary">
+            Create new project
+          </Button>
+        </div>
+      </Link>
+    </NavItem>
+  ))}
+</Nav>;
